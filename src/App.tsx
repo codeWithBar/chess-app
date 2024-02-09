76802b37913fba09ce92be52b7a5125fed45a451
Barlas Söker <bsoker98@gmail.com>
2024-02-09 13:31:42 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import PlayRandomMoveEngine from "./PlayRandomMoveEngine";
+
+function App() {
+  return (
+    <div
+      style={{ height: "50%", width: "40%", backgroundColor: "#00B1E1" }}
+      className="col-auto mx-5 my-5"
+    >
+      <PlayRandomMoveEngine></PlayRandomMoveEngine>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/PlayRandomMoveEngine.tsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import { Chess, ShortMove } from "chess.js";
+import { Chessboard } from "react-chessboard";
+
+export default function PlayRandomMoveEngine() {
+  const [game, setGame] = useState(new Chess());
+
+  function makeAMove(move: string | ShortMove) {
+    const gameCopy = { ...game };
+    const result = gameCopy.move(move);
+    setGame(gameCopy);
+    return result; // null if the move was illegal, the move object if the move was legal
+  }
+
+  // function makeRandomMove() {
+  //   const possibleMoves = game.moves();
+  //   if (game.game_over() || game.in_draw() || possibleMoves.length === 0)
+  //     return; // exit if the game is over
+  //   const randomIndex = Math.floor(Math.random() * possibleMoves.length);
+  //   makeAMove(possibleMoves[randomIndex]);
+  // }
+
+  function onDrop(sourceSquare: any, targetSquare: any) {
+    const move = makeAMove({
+      from: sourceSquare,
+      to: targetSquare,
+      promotion: "q", // always promote to a queen for example simplicity
+    });
+
+    // illegal move
+    if (move === null) return false;
+    // setTimeout(makeRandomMove, 200);
+    return true;
+  }
+
+  return (
+    <Chessboard
+      position={game.fen()}
+      showBoardNotation={true}
+      onPieceDrop={onDrop}
+    />
+  );
+}
